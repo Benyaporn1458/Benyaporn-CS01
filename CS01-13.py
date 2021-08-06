6e16af2ec4e41769944c414f8756f3301796b2f5
Benyaporn1458 <41430@virtual.prc.ac.th>
2021-08-06 09:16:39 +0700

    Update CS01-13.py

--- CS01-13.py
+++ CS01-13.py
@@ -1,8 +1,7 @@
-A=[]
+A = ()
 while (True):
-    i=int(input("ใส่เกรดของคุณ "))
+    i=float(input("ใส่เกรดของคุณ "))
+    print("เกรดของคุณคือ ",i)
     if i == -1 : 
        break
-    A += [i]
-    print("เกรดของคุณคือ ",i)
 print(A)
