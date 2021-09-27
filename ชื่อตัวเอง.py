772a34ad0863e62c96d914ffbf7f9f7f588fe62f
Benyaporn1458 <41430@virtual.prc.ac.th>
2021-09-27 13:50:48 +0700

    Delete ชื่อตัวเอง.py

--- ชื่อตัวเอง.py
+++
@@ -1,8 +0,0 @@
-from tkinter import*
-root = Tk()
-root.title("GUI")
-myText=Label(text="My name is ", fg="light blue",font=20).grid(row=0,column=0)
-myText=Label(text="Benyaporn", fg="light green",font=20).grid(row=1,column=1)
-myText=Label(text="Udomsilapasub ", fg="light pink",font=20).grid(row=2,column=2)
-root.geometry("400x400")
-root.mainloop()
